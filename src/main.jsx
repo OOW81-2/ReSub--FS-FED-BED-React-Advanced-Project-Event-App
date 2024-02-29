27e8b8fccf3a772d8c95c489caf3653d6ab6883a
OOW81 <118228607+OOW81@users.noreply.github.com>
2024-02-29 05:27:22 +0100

    add: page styling

---
+++ src/main.jsx
@@ -0,0 +1,52 @@
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { EventPage, loader as eventLoader } from "./pages/EventPage";
+import { EventsPage, loader as postListLoader } from "./pages/EventsPage";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { Root, loader as AppDataLoader } from "./components/Root";
+import { PageNotFound } from "./pages/PageNotFound";
+
+const theme = extendTheme({
+  fonts: {
+    heading: `'DM Sans', sans-serif`,
+    body: `'DM Sans', sans-serif`,
+  },
+  styles: {
+    global: {
+      body: {
+        background: "hsl(202, 81%, 85%)",
+        color: "hsl(214, 8%, 23%)",
+      },
+    },
+  },
+});
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    loader: AppDataLoader,
+    errorElement: <PageNotFound />,
+    children: [
+      {
+        path: "/",
+        element: <EventsPage />,
+        loader: postListLoader,
+      },
+      {
+        path: "/event/:eventId",
+        element: <EventPage />,
+        loader: eventLoader,
+      },
+    ],
+  },
+]);
+// @ts-ignore
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <ChakraProvider theme={theme}>
+      <RouterProvider router={router} />
+    </ChakraProvider>
+  </React.StrictMode>
+);
